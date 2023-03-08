467d3f7a89ffcfd32eabda591fe052d02c2ed65d
Mark L <marklse568@gmail.com>
2023-03-08 14:57:04 +0100

    Run formatter across project

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import { ReactSVG } from 'react-svg';
 
-function Logo() {
+export const Logo = () => {
   return (
     <ReactSVG
       src="icons/wheelchair.svg"
       className="p-1 w-10 stroke-lime-600 hover:stroke-lime-300"
     />
   );
-}
-
-export default Logo;
+};
