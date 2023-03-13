53c7f47ca018112aa1d58dfd7f65307054c2d533
Mark L <marklse568@gmail.com>
2023-03-13 21:53:28 +0100

    FTT-24: Change kc client id to be more descriptive

--- frontend/src/keycloak.ts
+++ frontend/src/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 const keycloak = new Keycloak({
   url: 'http://localhost:8080',
   realm: 'ftt',
-  clientId: 'ftt',
+  clientId: 'ftt_frontend',
 });
 
 export default keycloak;
