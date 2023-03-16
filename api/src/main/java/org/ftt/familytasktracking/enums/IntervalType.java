e13a6179eb6af4732ea33ad34c924087049b977d
Malek Hamza <malek.hamza@gmx.de>
2023-03-16 15:12:30 +0100

    FTT-25: Add Docs for Enums

--- api/src/main/java/org/ftt/familytasktracking/enums/IntervalType.java
+++ api/src/main/java/org/ftt/familytasktracking/enums/IntervalType.java
@@ -1,5 +1,8 @@
 package org.ftt.familytasktracking.enums;
 
+/**
+ * The interval type defines, as the name suggests, the interval of tasks combined with an integer.
+ */
 public enum IntervalType {
     HOURS,
     DAYS,
--- api/src/main/java/org/ftt/familytasktracking/enums/PermissionType.java
+++ api/src/main/java/org/ftt/familytasktracking/enums/PermissionType.java
@@ -1,5 +1,8 @@
 package org.ftt.familytasktracking.enums;
 
+/**
+ * The permission type defines the permission of a {@link org.ftt.familytasktracking.entities.Profile}
+ */
 public enum PermissionType {
     MEMBER,
     ADMIN
