6ad10864b0c53d13c47cf65c333a9b13e1f7124f
Malek Hamza <malek.hamza@gmx.de>
2023-03-22 10:13:58 +0100

    FTT-29: Add initial Keycloak Service

---
+++ api/src/main/java/org/ftt/familytasktracking/services/KeycloakServiceImpl.java
@@ -0,0 +1,43 @@
+package org.ftt.familytasktracking.services;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+/**
+ * Implementation of {@link KeycloakService}
+ */
+@Service
+public class KeycloakServiceImpl implements KeycloakService {
+    private static final Pattern UUID_REGEX =
+            Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+
+    /**
+     * Checks with Pattern Matcher {@link #UUID_REGEX} if the Jwt Subject contains a UUID.
+     *
+     * @param token {@link Jwt}-Object with Subject
+     * @return boolean if {@link Jwt#getSubject()} contains a UUID
+     */
+    @Override
+    public boolean isJwtSubjectContainingUUID(Jwt token) {
+        return UUID_REGEX.matcher(token.getSubject()).matches();
+    }
+
+    /**
+     * Fetches Keycloak User ID from Jwt and parses it as UUID
+     *
+     * @param token {@link Jwt}-Object with Subject
+     * @return {@link UUID} from {@link Jwt}, null if the Subject isn't containing a UUID.
+     */
+    @Override
+    public UUID getKeycloakUserId(Jwt token) {
+        if (isJwtSubjectContainingUUID(token)) {
+            String subject = token.getSubject();
+            return UUID.fromString(subject);
+        } else {
+            return null;
+        }
+    }
+}
