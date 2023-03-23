9a01e0cf9a11396ac8b9dce999d92525eb3cae82
mkh <Malek.Hamza@dbh.de>
2023-03-23 12:39:08 +0100

    FTT-29: Update ErrorDetails to store HttpStatus as int

--- api/src/main/java/org/ftt/familytasktracking/exceptions/ErrorDetails.java
+++ api/src/main/java/org/ftt/familytasktracking/exceptions/ErrorDetails.java
@@ -5,11 +5,11 @@ import org.springframework.http.HttpStatus;
 import java.time.LocalDateTime;
 
 public record ErrorDetails(
-        HttpStatus httpStatus,
+        int httpStatus,
         String message,
         LocalDateTime timestamp
 ) {
     public ErrorDetails(HttpStatus httpStatus, String message) {
-        this(httpStatus, message, LocalDateTime.now());
+        this(httpStatus.value(), message, LocalDateTime.now());
     }
 }
--- api/src/main/java/org/ftt/familytasktracking/exceptions/WebRtExecHandler.java
+++ api/src/main/java/org/ftt/familytasktracking/exceptions/WebRtExecHandler.java
@@ -1,5 +1,6 @@
 package org.ftt.familytasktracking.exceptions;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,6 @@ public class WebRtExecHandler {
     @ExceptionHandler(WebRtExec.class)
     @ResponseBody
     public ResponseEntity<?> handleWebRtExec(WebRtExec ex) {
-        return new ResponseEntity<>(ex.getErrorDetails(), ex.getErrorDetails().httpStatus());
+        return new ResponseEntity<>(ex.getErrorDetails(), HttpStatusCode.valueOf(ex.getErrorDetails().httpStatus()));
     }
 }
