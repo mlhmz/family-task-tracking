678093e428bfe8c7f22803743a738c88800c2298
mkh <Malek.Hamza@dbh.de>
2023-03-23 12:03:15 +0100

    FTT-29: Improve Service Code Quality

--- api/src/main/java/org/ftt/familytasktracking/services/HouseholdService.java
+++ api/src/main/java/org/ftt/familytasktracking/services/HouseholdService.java
@@ -12,25 +12,14 @@ import java.util.UUID;
  * Service-Interface for all Household Processes
  */
 public interface HouseholdService {
-    /**
-     * Gets a household by the linked Keycloak User to it that is identified by the jwt
-     *
-     * @param jwt {@link Jwt} of the Keycloak User
-     * @return {@link Household} that the user is linked to
-     */
     HouseholdResponseDto getHouseholdResponseByJwt(Jwt jwt);
 
     Optional<Household> getHouseholdByJwt(Jwt jwt);
 
     HouseholdResponseDto getHouseholdResponseByKeycloakUserId(UUID keycloakUserId);
 
-    /**
-     * Creates a Household, if a household already exists, it wont be created
-     *
-     * @param jwt                 of the household / that should be created with the household
-     * @param householdRequestDto Household Entity that should be saved
-     * @return {@link Household} as callback
-     */
+    Optional<Household> getHouseholdByKeycloakUserId(UUID keycloakUserId);
+
     HouseholdResponseDto createHouseholdByRequest(Jwt jwt, HouseholdRequestDto householdRequestDto);
 
     HouseholdResponseDto updateHouseholdByRequest(Jwt jwt, HouseholdRequestDto householdRequestDto);
--- api/src/main/java/org/ftt/familytasktracking/services/HouseholdServiceImpl.java
+++ api/src/main/java/org/ftt/familytasktracking/services/HouseholdServiceImpl.java
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.UUID;
 
+/**
+ * Service-Implementation of the {@link HouseholdService}
+ */
 @Service
 public class HouseholdServiceImpl implements HouseholdService {
     private final HouseholdRepository householdRepository;
@@ -26,31 +29,65 @@ public class HouseholdServiceImpl implements HouseholdService {
         this.keycloakService = keycloakService;
     }
 
+    /**
+     * Gets a household by the JWT of the linked Keycloak User
+     *
+     * @param jwt {@link Jwt} of the Keycloak User which contains the keycloakUserId as a subject
+     * @return {@link HouseholdResponseDto} that the user is linked to
+     * @throws ObjectNotFoundException if the household doesn't exist
+     */
     @Override
     public HouseholdResponseDto getHouseholdResponseByJwt(Jwt jwt) {
         UUID keycloakUserId = this.keycloakService.getKeycloakUserId(jwt);
         return getHouseholdResponseByKeycloakUserId(keycloakUserId);
     }
 
+    /**
+     * Gets a household by the Keycloak User ID of a certain user
+     *
+     * @param keycloakUserId UUID of the Household
+     * @return {@link HouseholdResponseDto} of the Keycloak User
+     * @throws ObjectNotFoundException if the household doesn't exist
+     */
     @Override
     public HouseholdResponseDto getHouseholdResponseByKeycloakUserId(UUID keycloakUserId) {
         Optional<Household> household = getHouseholdByKeycloakUserId(keycloakUserId);
         if (household.isEmpty()) {
-            throw new HouseholdAlreadyExistingException();
+            throw new ObjectNotFoundException();
         }
         return this.householdMapper.mapHouseholdToHouseholdResponseDto(household.get());
     }
 
+    /**
+     * Gets a household by the JWT of the linked Keycloak User
+     *
+     * @param jwt {@link Jwt} of the Keycloak User which contains the keycloakUserId as a subject
+     * @return {@link Household} that the user is linked to
+     */
     @Override
     public Optional<Household> getHouseholdByJwt(Jwt jwt) {
         UUID keycloakUserId = this.keycloakService.getKeycloakUserId(jwt);
         return getHouseholdByKeycloakUserId(keycloakUserId);
     }
 
+    /**
+     * Gets a household by the Keycloak User ID of a certain user
+     *
+     * @param keycloakUserId UUID of the Household
+     * @return {@link Household} of the Keycloak User
+     */
+    @Override
     public Optional<Household> getHouseholdByKeycloakUserId(UUID keycloakUserId) {
         return this.householdRepository.getHouseholdByKeycloakUserId(keycloakUserId);
     }
 
+    /**
+     * Creates a Household, if a household already exists, it won't be created
+     *
+     * @param jwt                 of the household / that should be created with the household
+     * @param householdRequestDto Household Entity that should be saved
+     * @return {@link Household} as callback
+     */
     @Override
     public HouseholdResponseDto createHouseholdByRequest(Jwt jwt, HouseholdRequestDto householdRequestDto) {
         UUID keycloakUserId = this.keycloakService.getKeycloakUserId(jwt);
@@ -63,6 +100,13 @@ public class HouseholdServiceImpl implements HouseholdService {
         return this.householdMapper.mapHouseholdToHouseholdResponseDto(savedHousehold);
     }
 
+    /**
+     * Updates a household with a {@link HouseholdRequestDto}-Object
+     *
+     * @param jwt                 {@link Jwt} that the Household is identified with
+     * @param householdRequestDto Dto with which the Household is updated with
+     * @return Updated {@link HouseholdResponseDto}-Object
+     */
     @Override
     public HouseholdResponseDto updateHouseholdByRequest(Jwt jwt, HouseholdRequestDto householdRequestDto) {
         Optional<Household> household = this.getHouseholdByJwt(jwt);
@@ -76,6 +120,12 @@ public class HouseholdServiceImpl implements HouseholdService {
         return this.householdMapper.mapHouseholdToHouseholdResponseDto(updatedHousehold);
     }
 
+    /**
+     * Saves a household into the repository
+     *
+     * @param household {@link Household} to save
+     * @return Saved {@link Household}
+     */
     @Override
     public Household saveHousehold(Household household) {
         return this.householdRepository.save(household);
