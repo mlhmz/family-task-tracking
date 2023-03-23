426d114eabd4d1792bc7107d606d6df084a9bc28
mkh <Malek.Hamza@dbh.de>
2023-03-23 12:07:13 +0100

    FTT-29: Create HouseholdController Docs

--- api/src/main/java/org/ftt/familytasktracking/controller/HouseholdController.java
+++ api/src/main/java/org/ftt/familytasktracking/controller/HouseholdController.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controller for Handling Household related Stuff
+ */
 @RestController
 @RequestMapping("/api/v1/household")
 public class HouseholdController {
@@ -17,19 +20,40 @@ public class HouseholdController {
         this.householdService = householdService;
     }
 
+    /**
+     * Gets a household by the JWT
+     *
+     * @param jwt {@link Jwt} that is automatically sent and fetched by the {@link AuthenticationPrincipal}
+     * @return {@link ResponseEntity} with the {@link HouseholdResponseDto}
+     */
     @GetMapping
     public ResponseEntity<HouseholdResponseDto> getHouseholdByJwt(@AuthenticationPrincipal Jwt jwt) {
         HouseholdResponseDto response = this.householdService.getHouseholdResponseByJwt(jwt);
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Creates a household with the JWT
+     *
+     * @param request {@link HouseholdRequestDto} to create a {@link org.ftt.familytasktracking.entities.Household} with
+     * @param jwt {@link Jwt} that is automatically sent and fetched by the {@link AuthenticationPrincipal}
+     * @return {@link ResponseEntity} with the {@link HouseholdResponseDto}
+     */
     @PostMapping
     public ResponseEntity<HouseholdResponseDto> createHouseholdByJwt(@RequestBody HouseholdRequestDto request,
                                                                      @AuthenticationPrincipal Jwt jwt) {
+        // TODO: When Creating a Household, a Profile should be created with it?
         HouseholdResponseDto response = this.householdService.createHouseholdByRequest(jwt, request);
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Updates a household with the JWT
+     *
+     * @param request {@link HouseholdRequestDto} to update a {@link org.ftt.familytasktracking.entities.Household} with
+     * @param jwt {@link Jwt} that is automatically sent and fetched by the {@link AuthenticationPrincipal}
+     * @return {@link ResponseEntity} with the {@link HouseholdResponseDto}
+     */
     @PutMapping
     public ResponseEntity<HouseholdResponseDto> updateHouseholdByJwt(@RequestBody HouseholdRequestDto request,
                                                                      @AuthenticationPrincipal Jwt jwt) {
