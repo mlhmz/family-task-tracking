db8fcadfb1abef18f17c7126b793776be500a11d
Malek Hamza <malek.hamza@gmx.de>
2023-04-01 19:35:40 +0200

    FTT-35: Delete Routes from Profile that unprivileged Users shouldn't use

--- api/src/main/java/org/ftt/familytasktracking/controller/ProfileController.java
+++ api/src/main/java/org/ftt/familytasktracking/controller/ProfileController.java
@@ -1,14 +1,15 @@
 package org.ftt.familytasktracking.controller;
 
-import org.ftt.familytasktracking.dtos.ProfileRequestDto;
 import org.ftt.familytasktracking.dtos.ProfileResponseDto;
 import org.ftt.familytasktracking.entities.Profile;
 import org.ftt.familytasktracking.models.ProfileModel;
 import org.ftt.familytasktracking.services.ProfileService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
@@ -53,53 +54,5 @@ public class ProfileController {
                                                      @AuthenticationPrincipal Jwt jwt) {
         ProfileModel model = this.profileService.getProfileByUuidAndJwt(uuid, jwt);
         return model.toResponseDto();
-    }
-
-    /**
-     * Creates a Profile and binds a {@link org.ftt.familytasktracking.entities.Household}
-     * with the {@link Jwt} to it
-     *
-     * @param profileRequestDto Content of the new Profile
-     * @param jwt of the Household that the Profile is assigned to
-     * @return {@link ProfileResponseDto} with the persisted Profile
-     */
-    @PostMapping
-    public ProfileResponseDto createProfileWithJwt(@RequestBody ProfileRequestDto profileRequestDto,
-                                                   @AuthenticationPrincipal Jwt jwt) {
-        ProfileModel model = this.profileService.buildModelFromProfileRequestDto(profileRequestDto);
-        ProfileModel persistedModel = this.profileService.createProfile(model, jwt);
-        return persistedModel.toResponseDto();
-    }
-
-    /**
-     * Updates a Profile identified by its {@link UUID} and by the {@link Jwt}
-     *
-     * @param uuid {@link Profile}'s identifier
-     * @param profileRequestDto Update Content
-     * @param jwt Household Identifier
-     * @return Persisted {@link ProfileResponseDto}
-     */
-    @PutMapping("/{uuid}")
-    public ProfileResponseDto updateProfileByUuidAndJwt(@PathVariable("uuid") UUID uuid,
-                                                        @RequestBody ProfileRequestDto profileRequestDto,
-                                                        @AuthenticationPrincipal Jwt jwt) {
-        ProfileModel updateModel = this.profileService.buildModelFromProfileRequestDto(profileRequestDto);
-        ProfileModel targetModel = this.profileService.getProfileByUuidAndJwt(uuid, jwt);
-        ProfileModel persistedModel = this.profileService.updateProfile(updateModel, targetModel);
-        return persistedModel.toResponseDto();
-    }
-
-    /**
-     * Deletes a Profile identified by its {@link UUID} and by the {@link Jwt}
-     *
-     * @param uuid Identifier of the Profile
-     * @param jwt Household Identifier
-     * @return No Content Response
-     */
-    @DeleteMapping("/{uuid}")
-    public ResponseEntity<?> deleteProfileByUuidAndJwt(@PathVariable("uuid") UUID uuid,
-                                                       @AuthenticationPrincipal Jwt jwt) {
-        this.profileService.deleteProfileByUuid(uuid, jwt);
-        return ResponseEntity.noContent().build();
     }
 }
