4fb84c9dfa9368f41e1f5b2dbd4a7d2e29a46ebe
Malek Hamza <malek.hamza@gmx.de>
2023-04-01 23:53:48 +0200

    FTT-35: Improve Model Doc

--- api/src/main/java/org/ftt/familytasktracking/models/Model.java
+++ api/src/main/java/org/ftt/familytasktracking/models/Model.java
@@ -1,13 +1,26 @@
 package org.ftt.familytasktracking.models;
 
 /**
- * Domain Model Object that stores the Entity and offers convert Methods
+ * Domain Model Object that represents an entity.
+ * <p>
+ * It makes the handling between the Controllers and Services easier by
+ * offering the Data as Dto and as Entity
  *
  * @param <T> Actual Entity Object
- * @param <U> Dto Object
+ * @param <U> Representative Object (DTO)
  */
 public interface Model<T, U> {
+    /**
+     * Gets the Entity from the {@link Model}
+     *
+     * @return {@link T} that is the actual entity
+     */
     T toEntity();
 
+    /**
+     * Maps the inner Entity to the Response Dto
+     *
+     * @return {@link U} that represents the Entity
+     */
     U toResponseDto();
 }
