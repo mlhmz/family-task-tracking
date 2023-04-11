5d2fcf5241695836cc599fb21ec9dd41771072a5
Malek Hamza <malek.hamza@gmx.de>
2023-04-11 21:29:23 +0200

    FTT-40: Extend TaskService and Repo

--- api/src/main/java/org/ftt/familytasktracking/repositories/TaskRepository.java
+++ api/src/main/java/org/ftt/familytasktracking/repositories/TaskRepository.java
@@ -1,9 +1,15 @@
 package org.ftt.familytasktracking.repositories;
 
+import org.ftt.familytasktracking.entities.Household;
 import org.ftt.familytasktracking.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findAllByHousehold(Household household);
+
+    Task findTaskByHouseholdAndUuid(Household household, UUID uuid);
+
 }
--- api/src/main/java/org/ftt/familytasktracking/services/TaskService.java
+++ api/src/main/java/org/ftt/familytasktracking/services/TaskService.java
@@ -1,8 +1,14 @@
 package org.ftt.familytasktracking.services;
 
+import lombok.NonNull;
 import org.ftt.familytasktracking.dtos.TaskRequestDto;
 import org.ftt.familytasktracking.dtos.TaskResponseDto;
+import org.ftt.familytasktracking.entities.Task;
+import org.ftt.familytasktracking.models.TaskModel;
 import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface TaskService {
     /**
@@ -12,4 +18,14 @@ public interface TaskService {
      * @return {@link TaskResponseDto}
      */
     TaskResponseDto createTask(TaskRequestDto dto, Jwt jwt);
+
+    List<TaskModel> getAllTasksByJwt(@NonNull Jwt jwt, Boolean done);
+
+    List<TaskModel> getAllTasksByJwtAndSearchQuery(@NonNull Jwt jwt, @NonNull String query);
+
+    TaskModel getTaskByUuidAndJwt(@NonNull UUID uuid, @NonNull Jwt jwt);
+
+    TaskModel buildModelFromTaskEntity(Task entity);
+
+    TaskModel buildModelFromTaskRequestDto(TaskRequestDto dto);
 }
