ec36c27b404da6d595df4506f71516bb43d2b171
Malek Hamza <malek.hamza@gmx.de>
2023-04-11 21:33:16 +0200

    FTT-40: Add Factory for Search Query Parsers

---
+++ api/src/main/java/org/ftt/familytasktracking/search/SearchQueryParserFactory.java
@@ -0,0 +1,29 @@
+package org.ftt.familytasktracking.search;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class SearchQueryParserFactory {
+    private static SearchQueryParserFactory instance;
+
+    public static SearchQueryParserFactory getInstance() {
+        if (instance == null) {
+            instance = new SearchQueryParserFactory();
+        }
+        return instance;
+    }
+
+    public <U> SearchQueryParser<U> getSearchQueryParserByDataType(SearchQuery searchQuery) {
+        if (searchQuery.value().getClass().equals(String.class)) {
+            return new StringSearchQueryParser<>();
+        } else if (searchQuery.value().getClass().equals(UUID.class)) {
+            return new DateTimeSearchQueryParser<>();
+        } else if (searchQuery.value().getClass().equals(Integer.class)) {
+            return new IntegerSearchQueryParser<>();
+        } else if (searchQuery.value().getClass().equals(LocalDateTime.class)) {
+            return new DateTimeSearchQueryParser<>();
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
