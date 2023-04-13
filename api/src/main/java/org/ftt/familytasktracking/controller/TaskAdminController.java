72e7f6af6fbf75ba9e3aa830257815c1087d7bb4
Malek Hamza <malek.hamza@gmx.de>
2023-04-13 11:52:59 +0200

    FTT-38: Add swagger docs to TaskAdminController.java

--- api/src/main/java/org/ftt/familytasktracking/controller/TaskAdminController.java
+++ api/src/main/java/org/ftt/familytasktracking/controller/TaskAdminController.java
@@ -1,9 +1,15 @@
 package org.ftt.familytasktracking.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import org.ftt.familytasktracking.dtos.ProfileResponseDto;
 import org.ftt.familytasktracking.dtos.TaskRequestDto;
 import org.ftt.familytasktracking.dtos.TaskResponseDto;
+import org.ftt.familytasktracking.exceptions.ErrorDetails;
 import org.ftt.familytasktracking.services.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +29,23 @@ public class TaskAdminController {
     }
 
     @Operation(summary = "Creates a new task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created task",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProfileResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid JSON submitted",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "401", description = "Request doesn't contain valid bearer token or " +
+                    "Session Id is invalid",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "The profile that is trying to create this resource is " +
+                    "unprivileged",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))})
+    }
+    )
     @PostMapping
     public ResponseEntity<TaskResponseDto> createTask(@RequestBody @Valid final TaskRequestDto dto,
                                                       @AuthenticationPrincipal Jwt jwt) {
@@ -30,6 +53,23 @@ public class TaskAdminController {
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Deletes a Task by it's UUID and it's Authorization-Token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Found task and deleted it",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Request doesn't contain valid bearer token or " +
+                    "Session Id is invalid",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "403", description = "The profile that is trying to delete this resource is " +
+                    "unprivileged",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}),
+            @ApiResponse(responseCode = "404", description = "Task to delete couldn't be found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorDetails.class))}
+            )}
+    )
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTaskById(@PathVariable("id") UUID id, @AuthenticationPrincipal Jwt jwt) {
