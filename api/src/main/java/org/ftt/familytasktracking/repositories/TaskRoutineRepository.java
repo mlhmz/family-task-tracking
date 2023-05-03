225d835ce8d1e527f2a95cf862a06ddf661aec5e
Malek Hamza <malek.hamza@gmx.de>
2023-05-03 10:02:48 +0200

    FTT-42: Remove TaskRoutine

--- api/src/main/java/org/ftt/familytasktracking/repositories/TaskRoutineRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ftt.familytasktracking.repositories;
-
-import org.ftt.familytasktracking.entities.TaskRoutine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TaskRoutineRepository extends JpaRepository<TaskRoutine, UUID> {
-}
