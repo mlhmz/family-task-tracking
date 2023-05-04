d5ab298918dba7f5683cf738f17cd2c64a1b4130
mul <moritz.ulrich.hb@googlemail.com>
2023-05-04 11:59:30 +0200

    FTT-45: code adjustments

--- api/src/main/java/org/ftt/familytasktracking/hooks/TaskFinishedUpdateHook.java
+++ api/src/main/java/org/ftt/familytasktracking/hooks/TaskFinishedUpdateHook.java
@@ -7,10 +7,6 @@ public class TaskFinishedUpdateHook implements TaskUpdateHook {
 
     @Override
     public void executeUpdateHook(Task updateTask, Task targetTask, boolean safe) {
-        setTaskFinished(targetTask);
-    }
-
-    public void setTaskFinished(Task targetTask) {
         if (TaskState.REVIEWED == targetTask.getTaskState()) {
             targetTask.setTaskState(TaskState.FINISHED);
         }
--- api/src/main/java/org/ftt/familytasktracking/hooks/TaskPointsUpdateHook.java
+++ api/src/main/java/org/ftt/familytasktracking/hooks/TaskPointsUpdateHook.java
@@ -6,14 +6,11 @@ import org.ftt.familytasktracking.enums.TaskState;
 public class TaskPointsUpdateHook implements TaskUpdateHook {
     @Override
     public void executeUpdateHook(Task updateTask, Task targetTask, boolean safe) {
-        givePointsForTaskReviewed(targetTask);
-    }
-
-    public void givePointsForTaskReviewed(Task targetTask) {
         if (TaskState.REVIEWED == targetTask.getTaskState()) {
             Integer points = targetTask.getAssignee().getPoints() + targetTask.getPoints();
             targetTask.getAssignee().setPoints(points);
         }
     }
+
 
 }
