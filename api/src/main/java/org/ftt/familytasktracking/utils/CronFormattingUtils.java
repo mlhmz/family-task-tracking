c2847b07fd1da0d2e23437a8acd68cb16342f18f
Malek Hamza <malek.hamza@gmx.de>
2023-05-04 13:42:37 +0200

    FTT-41: Add additional Null Check for CronExpression in Utils

--- api/src/main/java/org/ftt/familytasktracking/utils/CronFormattingUtils.java
+++ api/src/main/java/org/ftt/familytasktracking/utils/CronFormattingUtils.java
@@ -1,5 +1,7 @@
 package org.ftt.familytasktracking.utils;
 
+import io.micrometer.common.util.StringUtils;
+
 import java.time.LocalDateTime;
 
 public class CronFormattingUtils {
@@ -7,13 +9,18 @@ public class CronFormattingUtils {
     }
 
     public static String format(String cronExpression) {
-        LocalDateTime dateTime = LocalDateTime.now();
-        return cronExpression
-                .replace("${CS}", Integer.toString(dateTime.getSecond()))
-                .replace("${CM}", Integer.toString(dateTime.getMinute()))
-                .replace("${CH}", Integer.toString(dateTime.getHour()))
-                .replace("${CD}", Integer.toString(dateTime.getDayOfMonth()))
-                .replace("${CMT}", Integer.toString(dateTime.getMonthValue()))
-                .replace("${CDOW}", Integer.toString(dateTime.getDayOfWeek().getValue()));
+        if (StringUtils.isNotEmpty(cronExpression)) {
+            LocalDateTime dateTime = LocalDateTime.now();
+            return cronExpression
+                    .replace("${CS}", Integer.toString(dateTime.getSecond()))
+                    .replace("${CM}", Integer.toString(dateTime.getMinute()))
+                    .replace("${CH}", Integer.toString(dateTime.getHour()))
+                    .replace("${CD}", Integer.toString(dateTime.getDayOfMonth()))
+                    .replace("${CMT}", Integer.toString(dateTime.getMonthValue()))
+                    .replace("${CDOW}", Integer.toString(dateTime.getDayOfWeek().getValue()));
+        } else {
+            return null;
+        }
+
     }
 }
