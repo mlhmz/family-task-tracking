fd620d958ca2e0fece90bf646324e1cf136dd729
Malek Hamza <malek.hamza@gmx.de>
2023-05-05 23:30:00 +0200

    FTT-41: Implement a Milli Interval Task Scheduler

---
+++ api/src/main/java/org/ftt/familytasktracking/tasks/scheduler/MilliIntervalTaskScheduler.java
@@ -0,0 +1,17 @@
+package org.ftt.familytasktracking.tasks.scheduler;
+
+import org.ftt.familytasktracking.entities.Task;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class MilliIntervalTaskScheduler implements TaskScheduler {
+    @Override
+    public LocalDateTime getNextExecution(Task task) {
+        if (task.getIntervalMillis() == null) {
+            return null;
+        }
+        LocalDateTime lastTaskCreationAt = task.getLastTaskCreationAt();
+        return lastTaskCreationAt.plus(task.getIntervalMillis(), ChronoUnit.MILLIS);
+    }
+}
