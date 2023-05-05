6dfd71cb4dbd6f0a5c29f2cf3d80bf18fac4a656
Malek Hamza <malek.hamza@gmx.de>
2023-05-05 23:29:49 +0200

    FTT-41: Clean up CronTaskScheduler

--- api/src/main/java/org/ftt/familytasktracking/tasks/scheduler/CronTaskScheduler.java
+++ api/src/main/java/org/ftt/familytasktracking/tasks/scheduler/CronTaskScheduler.java
@@ -4,36 +4,17 @@ import io.micrometer.common.util.StringUtils;
 import org.ftt.familytasktracking.entities.Task;
 import org.ftt.familytasktracking.utils.CronFormattingUtils;
 import org.springframework.scheduling.support.CronExpression;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Component
 public class CronTaskScheduler implements TaskScheduler {
     @Override
-    public boolean rescheduleOnExpiration(Task task) {
-        boolean rescheduled = true;
+    public LocalDateTime getNextExecution(Task task) {
         String formattedExp = getFormattedCronExpressionString(task.getCronExpression());
         if (!isTaskScheduledAndExpValid(task, formattedExp)) {
-            rescheduled = false;
+            return null;
         }
-        CronExpression cronExpression = CronExpression.parse(formattedExp);
-        LocalDateTime nextExecution = cronExpression.next(getLastTaskCreation(task));
-        if (LocalDateTime.now().isAfter(nextExecution)) {
-            updateTaskSchedulingParametersByCronExpression(task, cronExpression);
-        } else {
-            rescheduled = false;
-        }
-        return rescheduled;
-    }
-
-    private LocalDateTime getLastTaskCreation(Task task) {
-        LocalDateTime lastTaskCreationAt = task.getLastTaskCreationAt();
-        if (lastTaskCreationAt == null) {
-            return task.getCreatedAt();
-        } else {
-            return lastTaskCreationAt;
-        }
+        return CronExpression.parse(formattedExp).next(task.getLastTaskCreationAt());
     }
 
     private String getFormattedCronExpressionString(String cronExpression) {
@@ -41,13 +22,6 @@ public class CronTaskScheduler implements TaskScheduler {
     }
 
     private boolean isTaskScheduledAndExpValid(Task task, String cronExpression) {
-        return Boolean.TRUE.equals(task.getScheduled()) &&
-                StringUtils.isNotEmpty(task.getCronExpression()) &&
-                CronExpression.isValidExpression(cronExpression);
-    }
-
-    private void updateTaskSchedulingParametersByCronExpression(Task task, CronExpression cronExpression) {
-        task.setLastTaskCreationAt(LocalDateTime.now());
-        task.setNextTaskCreationAt(cronExpression.next(LocalDateTime.now()));
+        return StringUtils.isNotEmpty(task.getCronExpression()) && CronExpression.isValidExpression(cronExpression);
     }
 }
