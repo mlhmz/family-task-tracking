57c07a0a69626fc3e4656d31966336fc06b4700c
Malek Hamza <malek.hamza@gmx.de>
2023-05-05 23:22:12 +0200

    FTT-41: Modify TaskScheduler API by only returning the next Execution

--- api/src/main/java/org/ftt/familytasktracking/tasks/scheduler/TaskScheduler.java
+++ api/src/main/java/org/ftt/familytasktracking/tasks/scheduler/TaskScheduler.java
@@ -2,11 +2,13 @@ package org.ftt.familytasktracking.tasks.scheduler;
 
 import org.ftt.familytasktracking.entities.Task;
 
+import java.time.LocalDateTime;
+
 /**
  * Interface for Task Rescheduling Logic
  * <br>
  * The implementation is injected by the {@link org.springframework.stereotype.Component} annotation
  */
 public interface TaskScheduler {
-    boolean rescheduleOnExpiration(Task task);
+    LocalDateTime getNextExecution(Task task);
 }
