6a2158f30f8c67cc57e0ecae2cf7dc93f89a58ac
Malek Hamza <malek.hamza@gmx.de>
2023-05-11 10:13:19 +0200

    Use ApiHandler type, add body and status to fetch

--- frontend/app/api/v1/[...segments]/route.ts
+++ frontend/app/api/v1/[...segments]/route.ts
@@ -1,45 +1,29 @@
+import { getJSONResponse, getNoContentResponse } from "@/lib/utils";
+import { ApiHandler } from "@/types/handlers";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-function setResponseHeaders(headers: Headers, originHeaders: Headers) {
-  headers.set("WWW-Authenticate", originHeaders.get("WWW-Authenticate") ?? "");
-  if (originHeaders.has("date")) {
-    headers.set("date", originHeaders.get("date") ?? `${new Date().toISOString}`)
-  }
-}
-
-async function getResponseContent(householdResponse: Response): Promise<any> {
-  try {
-    const jsonContent = await Promise.resolve(householdResponse.json());
-    return jsonContent;
-  } catch (err) {
-    return "";
-  }
-}
-
-// Check https://nextjs.org/docs/app/api-reference/file-conventions/route
-//  NextResponse != NextApiHandler (NextApiRequest, NextApiResponse): 
-//  NextRequest & Response ist das neue NextApiRequest & Response
-//  In der Doc wird NextResponse als Return Value benutzt, hier NextApiHandler
-//  NextApiHandler arbeitet nicht mit Context
-//  TODO: Typesafety
-const springHandler = async (req, context: { params }) => {
-  const jwtLiteral = await getToken({ req, secret: process.env.NEXTAUTH_SECRET, raw: true });
+const springHandler: ApiHandler = async (request, context) => {
+  const jwtLiteral = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET, raw: true });
   if (jwtLiteral) {
     const token = jwtLiteral.split(":")[0];
-    const url: string = context.params['segments'].join("/");
-    const householdResponse = await fetch(`${process.env.BACKEND_API_URL}/${url}`, {
+    const uri = context.params['segments'].join("/");
+    const householdResponse = await fetch(`${process.env.BACKEND_API_URL}/${uri}`, {
       headers: {
         Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
       },
+      body: request.body,
+      method: request.method,
+      // @ts-ignore
+      duplex: 'half'
     })
     try {
-      const response = NextResponse.json(
-        await getResponseContent(householdResponse),
-        { status: householdResponse.status },
-      );
-      setResponseHeaders(response.headers, householdResponse.headers);
-      return response;
+      if (householdResponse.status != 204) {
+        return await getJSONResponse(householdResponse);
+      } else {
+        return getNoContentResponse(householdResponse);
+      }
     } catch (error) {
       return NextResponse.json({ error: `${error}` }, { status: 500 });
     }
@@ -48,5 +32,4 @@ const springHandler = async (req, context: { params }) => {
   }
 };
 
-export { springHandler as GET, springHandler as POST, springHandler as PUT, springHandler as DELETE };
-
+export { springHandler as GET, springHandler as POST, springHandler as PUT, springHandler as PATCH, springHandler as DELETE };
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { NextResponse } from "next/server";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +8,35 @@ export function cn(...inputs: ClassValue[]) {
 
 export function parseJwt(token: string) {
   return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+}
+
+function setResponseHeaders(headers: Headers, originHeaders: Headers) {
+  headers.set("WWW-Authenticate", originHeaders.get("WWW-Authenticate") ?? "");
+  if (originHeaders.has("date")) {
+    headers.set("date", originHeaders.get("date") ?? `${new Date().toISOString}`)
+  }
+}
+
+async function getResponseContent(householdResponse: Response): Promise<any> {
+  try {
+    const jsonContent = await Promise.resolve(householdResponse.json());
+    return jsonContent;
+  } catch (err) {
+    return "";
+  }
+}
+
+export async function getJSONResponse(originResponse: Response) {
+  const response = NextResponse.json(
+    await getResponseContent(originResponse),
+    { status: originResponse.status }
+  );
+  setResponseHeaders(response.headers, originResponse.headers);
+  return response;
+}
+
+export function getNoContentResponse(originResponse: Response) {
+  const response = NextResponse.json({ message: "204 No content" }, { status: 200 });
+  setResponseHeaders(response.headers, originResponse.headers);
+  return response;
 }
