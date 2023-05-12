1a59d77cc529944c674ec727a4afee6cad545a43
Malek Hamza <malek.hamza@gmx.de>
2023-05-12 22:46:44 +0200

    Implement Profile Types

--- frontend/types/profile.ts
+++ frontend/types/profile.ts
@@ -1,7 +1,27 @@
 import { PermissionType } from "./permission-type";
 
 export interface ProfileRequest {
-    name?: string,
-    points?: number,
-    permissionType?: PermissionType
+  name?: string;
+  points?: number;
+  permissionType?: PermissionType;
+}
+
+export interface ProfileResponse {
+  uuid?: string;
+  sessionId?: string;
+  name?: string;
+  points?: number;
+  permissionType?: PermissionType;
+  passwordProtected?: boolean;
+}
+
+export interface ProfileAuthRequest {
+  uuid?: string;
+  password?: string;
+}
+
+export interface ProfileAuthResponse {
+  sessionId?: string;
+  profileId?: string;
+  expiresAt?: Date;
 }
