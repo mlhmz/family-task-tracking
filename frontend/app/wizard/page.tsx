d8958082b637d2ef9b82ab8c468291fce12dbc88
Malek Hamza <malek.hamza@gmx.de>
2023-05-14 17:11:38 +0200

    Improve style of greeter

--- frontend/app/wizard/page.tsx
+++ frontend/app/wizard/page.tsx
@@ -4,7 +4,7 @@ import { buttonVariants } from "@/components/ui/button";
 
 export default function WizardPage() {
   return (
-    <div className="m-auto flex flex-col gap-2">
+    <div className="m-auto my-5 flex w-1/3 flex-col gap-5">
       <h1 className="m-auto text-6xl">👋</h1>
       <h2 className="text-center text-2xl font-bold">Welcome to FTT</h2>
       <Link href="/wizard/1" className={buttonVariants({ size: "sm" })}>
