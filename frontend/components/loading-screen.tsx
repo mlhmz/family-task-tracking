29ed9fdae0164a95ec43ce73f0fe8a813209e552
Malek Hamza <malek.hamza@gmx.de>
2023-05-14 17:44:20 +0200

    Improve loading screen styling

--- frontend/components/loading-screen.tsx
+++ frontend/components/loading-screen.tsx
@@ -4,8 +4,10 @@ interface LoadingScreenProps {
 
 export function LoadingScreen({ textContent }: LoadingScreenProps) {
   return (
-    <div>
-      <h1>{textContent}</h1>
+    <div className="grid h-screen place-items-center">
+      <div>
+        <h1 className="text-2xl font-semibold">{textContent}</h1>
+      </div>
     </div>
   );
 }
