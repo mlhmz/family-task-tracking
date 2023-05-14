c889d34768140687c2741ff0bc88027c969b51d0
Malek Hamza <malek.hamza@gmx.de>
2023-05-14 22:01:32 +0200

    FTT-58: Add search queries for Rewards

--- api/src/main/java/org/ftt/familytasktracking/repositories/RewardRepository.java
+++ api/src/main/java/org/ftt/familytasktracking/repositories/RewardRepository.java
@@ -1,13 +1,17 @@
 package org.ftt.familytasktracking.repositories;
 
 import org.ftt.familytasktracking.entities.Household;
+import org.ftt.familytasktracking.entities.QReward;
 import org.ftt.familytasktracking.entities.Reward;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface RewardRepository extends JpaRepository<Reward, UUID> {
+public interface RewardRepository extends JpaRepository<Reward, UUID>,
+        QuerydslPredicateExecutor<Reward>, QuerydslBinderCustomizer<QReward> {
     List<Reward> findAllByHousehold(Household household);
 
     Reward findRewardByHouseholdAndUuid(Household household, UUID uuid);
