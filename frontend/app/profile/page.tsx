6c5ab4833a140e8aa53aae8a576b4f2213b0b2ac
Malek <malek.hamza@gmx.de>
2023-05-16 22:57:47 +0200

    Improve PermissionType Render

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -2,21 +2,27 @@
 
 import { useContext } from "react";
 
+import { getTranslatedPTValue } from "@/types/permission-type";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 import { ProfileContext } from "../profile-context";
 
 export default function Profile() {
-  const profile = useContext(ProfileContext);
+  const { profileInstance } = useContext(ProfileContext);
 
   return (
     <div className="m-auto my-5 flex w-1/3 flex-col gap-5">
-      <img className="m-auto rounded-md" src={profile.profileInstance.imageUrl} />
-      <h1 className="text-2xl font-bold">{profile.profileInstance.name}'s Profile</h1>
+      <img className="m-auto rounded-md" src={profileInstance.imageUrl} />
+      <h1 className="text-2xl font-bold">{profileInstance.name}'s Profile</h1>
       <div id="badges" className="flex flex-row gap-1">
-        <Badge variant={"secondary"}>{profile.profileInstance.permissionType}</Badge>
-        <Badge variant={"secondary"}>{profile.profileInstance.points} Points</Badge>
+        <Badge variant={"secondary"}>
+          {profileInstance.permissionType
+            ? getTranslatedPTValue(profileInstance.permissionType)
+            : "Unavailable"}
+        </Badge>
+        <Badge variant={"secondary"}>{profileInstance.points} Points</Badge>
       </div>
       <Button>Edit profile</Button>
     </div>
--- frontend/types/permission-type.ts
+++ frontend/types/permission-type.ts
@@ -2,3 +2,11 @@ export enum PermissionType {
   Admin = "ADMIN",
   Member = "MEMBER",
 }
+
+export function getTranslatedPTValue(permissionType: PermissionType) {
+  if (permissionType == PermissionType.Admin) {
+    return "Privileged";
+  } else {
+    return "Unprivileged"
+  }
+}
