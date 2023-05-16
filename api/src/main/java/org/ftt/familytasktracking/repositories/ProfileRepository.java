fb7528aa3087844571bbe81f78c7f9259d7c8e5a
Malek Hamza <malek.hamza@gmx.de>
2023-05-16 20:02:00 +0200

    FTT-51: Search-Queries for Profiles

--- api/src/main/java/org/ftt/familytasktracking/repositories/ProfileRepository.java
+++ api/src/main/java/org/ftt/familytasktracking/repositories/ProfileRepository.java
@@ -1,9 +1,17 @@
 package org.ftt.familytasktracking.repositories;
 
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import lombok.NonNull;
 import org.ftt.familytasktracking.entities.Profile;
+import org.ftt.familytasktracking.entities.QProfile;
 import org.ftt.familytasktracking.enums.PermissionType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +20,14 @@ import java.util.UUID;
 /**
  * Repository for the Profile entity.
  */
-public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+public interface ProfileRepository extends JpaRepository<Profile, UUID>,
+        QuerydslPredicateExecutor<Profile>, QuerydslBinderCustomizer<QProfile> {
+    @Override
+    default void customize(QuerydslBindings bindings, @NonNull QProfile root) {
+        bindings.bind(String.class)
+                .first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
+
     @Query("select p from Profile p where p.household.keycloakUserId = ?1")
     List<Profile> findAllByKeycloakUserId(UUID keycloakUserId);
 
@@ -24,6 +39,4 @@ public interface ProfileRepository extends JpaRepository<Profile, UUID> {
 
     @Query("select (count(p) > 0) from Profile p where p.household.keycloakUserId = ?1 and p.permissionType = ?2")
     boolean existsByKeycloakUserIdAndPermissionType(UUID keycloakUserId, PermissionType type);
-
-    Profile getProfileByUuid(UUID uuid);
 }
