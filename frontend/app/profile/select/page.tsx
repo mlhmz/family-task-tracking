ede2867f552eb3ba634e54298002afed3f1ddbb5
Malek Hamza <malek.hamza@gmx.de>
2023-05-17 21:41:01 +0200

    Fix profile select map error on empty profiles

--- frontend/app/profile/select/page.tsx
+++ frontend/app/profile/select/page.tsx
@@ -23,7 +23,7 @@ export default function ProfileAuth() {
     <div className="m-auto my-5 flex w-full flex-col gap-2">
       <h1 className="text-center text-2xl font-bold">Select a Profile</h1>
       <div className="flex flex-wrap justify-center gap-2">
-        {data?.map((profile) => (
+        {Array.isArray(data) && data?.map((profile) => (
           <ProfileSelector profile={profile} key={profile.uuid} />
         ))}
       </div>
