17df06eb9c8fc18311810cd0648036321eb16db3
Mark L <marklse568@gmail.com>
2023-05-17 13:13:42 +0200

    Refactor second wizard page

--- frontend/app/wizard/2/page.tsx
+++ frontend/app/wizard/2/page.tsx
@@ -1,21 +1,31 @@
 "use client";
 
-import { useState } from "react";
-
 import { useRouter } from "next/navigation";
 
-import { PermissionType } from "@/types/permission-type";
-import { Profile, ProfileAuthRequest, ProfileRequest } from "@/types/profile";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { z } from "zod";
+
+import { ProfileAuthRequest, ProfileRequest } from "@/types/profile";
+
+import { assertIsProfile } from "@/lib/guards";
+
+import { useZodForm } from "@/hooks/use-zod-form";
 
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 
 async function authProfile(profileAuthRequest: ProfileAuthRequest) {
-  await fetch("/api/v1/profiles/auth", {
+  const response = await fetch("/api/v1/profiles/auth", {
     method: "POST",
     body: JSON.stringify(profileAuthRequest),
   });
+
+  if (!response.ok) {
+    const error = await response.json();
+    if (error.message) throw new Error(error.message);
+    throw new Error("Problem authenticating profile");
+  }
 }
 
 async function createProfile(profileRequest: ProfileRequest) {
@@ -27,44 +37,67 @@ async function createProfile(profileRequest: ProfileRequest) {
     method: "POST",
     body: JSON.stringify(profileRequest),
   });
-  const profileResponse = (await response.json()) as Profile;
-  return profileResponse;
+
+  if (!response.ok) {
+    const error = await response.json();
+    if (error.message) throw new Error(error.message);
+    throw new Error("Problem creating profile");
+  }
+
+  const profile = await response.json();
+  assertIsProfile(profile);
+  return profile;
 }
 
-export default function SecondWizardPage() {
-  const [profileRequest, setProfileRequest] = useState<ProfileRequest>({
-    name: "",
-    permissionType: PermissionType.Admin,
-  } as ProfileRequest);
-  const router = useRouter();
+const schema = z.object({
+  name: z.string().min(1).max(255),
+});
 
-  const onAdministratorNameInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setProfileRequest({ name: e.target.value, permissionType: profileRequest.permissionType });
-  };
+export default function SecondWizardPage() {
+  const router = useRouter();
+  const { register, handleSubmit, formState } = useZodForm({ schema });
+  const {
+    data,
+    mutate,
+    isSuccess: isMutateSuccess,
+    isLoading,
+    error,
+  } = useMutation({
+    mutationFn: createProfile,
+  });
+
+  const { error: authError } = useQuery({
+    queryKey: ["profile", "auth"],
+    queryFn: () => data && authProfile({ profileUuid: data.uuid }),
+    onSuccess: () => {
+      router.push("/wizard/2");
+    },
+    enabled: isMutateSuccess,
+  });
+
+  const onSubmit = (formData: ProfileRequest) => mutate(formData);
 
   return (
     <div className="m-auto my-5 flex w-1/3 flex-col gap-5">
       <h1 className="m-auto text-6xl">👻</h1>
       <h3>Step 2</h3>
       <h2 className="text-2xl font-bold">Create an administrator profile</h2>
-      <Input
-        placeholder="Administrators Name"
-        onChange={onAdministratorNameInputChange}
-        value={profileRequest.name}
-        maxLength={255}
-      />
-      <Progress className="m-auto h-2 w-1/2" value={50}></Progress>
-      <Button
-        className={buttonVariants({ size: "sm" })}
-        onClick={() =>
-          createProfile(profileRequest).then((response) => {
-            authProfile({ profileUuid: response.uuid }).then((res) => {
-              router.push("/wizard/3");
-            });
-          })
-        }>
-        Next
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="flex flex-col items-center gap-3" disabled={isLoading}>
+          <Input placeholder="Administrator Name" {...register("name")} />
+          {formState.errors.name && <p className="text-destructive">{formState.errors.name.message}</p>}
+          <Progress className="m-auto h-2 w-1/2" value={50}></Progress>
+          <Button className={buttonVariants({ size: "sm" })} type="submit">
+            Next
+          </Button>
+          <>
+            {error && error instanceof Error && <p className="text-destructive">{error.message}</p>}
+            {authError && authError instanceof Error && (
+              <p className="text-destructive">{authError.message}</p>
+            )}
+          </>
+        </fieldset>
+      </form>
     </div>
   );
 }
--- frontend/lib/guards.ts
+++ frontend/lib/guards.ts
@@ -1,6 +1,7 @@
 "use client";
 
 import { HouseholdResponse } from "@/types/household";
+import { Profile } from "@/types/profile";
 
 export function assertIsHouseholdResponse(response: any): asserts response is HouseholdResponse {
   if (!response) {
@@ -20,5 +21,32 @@ export function assertIsHouseholdResponse(response: any): asserts response is Ho
   }
   if (response.updatedAt && typeof response.updatedAt !== "string") {
     throw new Error("HouseholdResponse.updatedAt is not a string");
+  }
+}
+
+export function assertIsProfile(response: any): asserts response is Profile {
+  if (!response) {
+    throw new Error("Profile is undefined");
+  }
+  if (typeof response !== "object") {
+    throw new Error("Profile is not an object");
+  }
+  if (response.uuid && typeof response.uuid !== "string") {
+    throw new Error("Profile.uuid is not a string");
+  }
+  if (response.sessionId && typeof response.sessionId !== "string") {
+    throw new Error("Profile.sessionId is not a string");
+  }
+  if (response.name && typeof response.name !== "string") {
+    throw new Error("Profile.name is not a string");
+  }
+  if (response.points && typeof response.points !== "number") {
+    throw new Error("Profile.points is not a number");
+  }
+  if (response.permissionType && typeof response.permissionType !== "string") {
+    throw new Error("Profile.permissionType is not a string");
+  }
+  if (response.passwordProtected && typeof response.passwordProtected !== "boolean") {
+    throw new Error("Profile.passwordProtected is not a boolean");
   }
 }
