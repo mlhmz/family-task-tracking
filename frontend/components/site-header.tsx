1027041780d8e4c74ca03487e5bbfee82e530325
marklse568 <marklse568@gmail.com>
2023-05-26 11:35:47 +0200

    Install sonner toaster

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -26,6 +26,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Toaster } from "sonner";
 
 export function SiteHeader() {
   const { status } = useSession();
@@ -39,6 +40,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <Toaster richColors={true} closeButton={true} />
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
