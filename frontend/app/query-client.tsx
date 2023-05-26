ec9922776a5cd31ae6d447cb28b31218b6dc86d3
marklse568 <marklse568@gmail.com>
2023-05-26 11:46:22 +0200

    Toast background refetch errors

--- frontend/app/query-client.tsx
+++ frontend/app/query-client.tsx
@@ -2,9 +2,20 @@
 
 import React from "react";
 
-import { QueryClientProvider as InternalProvider, QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider as InternalProvider, QueryCache, QueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.state.data !== undefined) {
+        toast.error(
+          `Something went wrong: ${error instanceof Error ? error.message : "Unknown server error"}`,
+        );
+      }
+    },
+  }),
+});
 
 export default function QueryClientProvider({ children }: { children: React.ReactNode }) {
   return <InternalProvider client={queryClient}>{children}</InternalProvider>;
