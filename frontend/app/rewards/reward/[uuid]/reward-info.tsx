e1132f12df85b080ad82f050bbdb5f6b4ab2fdeb
Malek <malek.hamza@gmx.de>
2023-05-31 10:56:43 +0200

    Remove accordions from reward info

--- frontend/app/rewards/reward/[uuid]/reward-info.tsx
+++ frontend/app/rewards/reward/[uuid]/reward-info.tsx
@@ -2,54 +2,42 @@ import { Reward } from "@/types/reward";
 
 import { formatISODateToReadable } from "@/lib/utils";
 
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Separator } from "@/components/ui/separator";
 
 export default function RewardInfo({ reward }: { reward: Reward }) {
   return (
-    <div className="flex flex-col items-center">
-      <div className="mx-3 w-full">
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Costs</AccordionTrigger>
-            <AccordionContent>
-              <div className="flex flex-row gap-1">
-                <p className="text-3xl">{reward?.cost}</p>
-                <p className="self-end">Points</p>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Description</AccordionTrigger>
-            <AccordionContent>
-              <p>{reward?.description}</p>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Timestamps</AccordionTrigger>
-            <AccordionContent>
-              <div className="flex flex-row items-center justify-center gap-5">
-                <div className="flex flex-col items-center">
-                  <h1 className="text-sm font-bold">Created At</h1>
-                  <p className="text-center">{formatISODateToReadable(reward?.createdAt)}</p>
-                </div>
-                <div className="flex flex-col items-center">
-                  <h1 className="text-sm font-bold">Updated At</h1>
-                  <p>{formatISODateToReadable(reward?.updatedAt)}</p>
-                </div>
-                {reward.redeemed && (
-                  <div className="flex flex-col items-center">
-                    <h1 className="text-sm font-bold">Redeemed At</h1>
-                    <p>{formatISODateToReadable(reward?.redeemedAt)}</p>
-                  </div>
-                )}
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+    <div className="flex flex-col items-center gap-5">
+      <div className="flex w-full flex-col gap-5">
+        <h3 className="font-bold">Costs</h3>
+        <div className="flex flex-row gap-1">
+          <p className="text-3xl">{reward?.cost}</p>
+          <p className="self-end">Points</p>
+        </div>
+      </div>
+      <Separator />
+      <div className="flex w-full flex-col gap-5">
+        <h3 className="font-bold">Description</h3>
+        <p>{reward?.description}</p>
+      </div>
+      <Separator />
+      <div className="flex w-full flex-col gap-5">
+        <h3 className="font-bold">Timestamps</h3>
+        <div className="flex flex-row items-center justify-center gap-5">
+          <div className="flex flex-col items-center">
+            <h1 className="text-sm font-bold">Created At</h1>
+            <p className="text-center">{formatISODateToReadable(reward?.createdAt)}</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="text-sm font-bold">Updated At</h1>
+            <p>{formatISODateToReadable(reward?.updatedAt)}</p>
+          </div>
+          {reward.redeemed && (
+            <div className="flex flex-col items-center">
+              <h1 className="text-sm font-bold">Redeemed At</h1>
+              <p>{formatISODateToReadable(reward?.redeemedAt)}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
