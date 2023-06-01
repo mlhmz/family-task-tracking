c89e494997ae63e70fd1f75a25eba6d757b0eeb3
Malek Hamza <malek.hamza@gmx.de>
2023-06-01 13:55:42 +0200

    Update App Name to be Parameterizable

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,7 +1,9 @@
+import { env } from "@/env.mjs";
+
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Questie",
+  name: env.NEXT_PUBLIC_APP_NAME,
   description: "Turn chores into fun sidequests while gaining rewards",
   mainNav: [
     {
