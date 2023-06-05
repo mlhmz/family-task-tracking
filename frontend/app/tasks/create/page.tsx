8da487a2af101183bca2d88bafd3b120b9e20acb
Malek Hamza <malek.hamza@gmx.de>
2023-06-05 13:00:42 +0200

    Update CreateTaskPage to be responsive

--- frontend/app/tasks/create/page.tsx
+++ frontend/app/tasks/create/page.tsx
@@ -2,7 +2,7 @@ import TaskCreateForm from "@/app/tasks/create/task-create-form";
 
 export default function CreateTaskPage() {
   return (
-    <div className="m-auto my-5 flex w-1/3 flex-col gap-5">
+    <div className="m-auto my-5 flex md:w-1/3 flex-col gap-5">
       <h1 className="text-center text-2xl font-bold">Create a Task</h1>
       <TaskCreateForm />
     </div>
