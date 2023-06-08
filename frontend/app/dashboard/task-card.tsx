3b41b7dfdba7daffe6a075d613d1c79aa3e72bc5
Malek Hamza <malek.hamza@gmx.de>
2023-06-08 14:50:16 +0200

    Add task button to dashboard and data table

--- frontend/app/dashboard/task-card.tsx
+++ frontend/app/dashboard/task-card.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 
-import { Task } from "@/types/task";
-import { getTranslatedTaskStateValue } from "@/types/task-state";
 import { Badge } from "@/components/ui/badge";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { PermissionType } from "@/types/permission-type";
+import { Task } from "@/types/task";
+import { getTranslatedTaskStateValue } from "@/types/task-state";
+import { useContext } from "react";
+import { ProfileContext } from "../profile-context";
+import RedeemTaskButton from "../tasks/redeem-task-button";
 
 export const TaskCard = ({ task }: { task: Task }) => {
+  const { data: profile } = useContext(ProfileContext);
+
   return (
     <Card className="min-h-[16.25rem] w-1/6 min-w-[13.875rem]">
       <Link href={`/tasks/task/${task.uuid}`}>
@@ -22,8 +28,11 @@ export const TaskCard = ({ task }: { task: Task }) => {
               : task.description}
           </p>
         </CardContent>
-        <CardFooter>
+        <CardFooter className="flex gap-1">
           <Badge>{task.points} Points</Badge>
+          { profile?.permissionType === PermissionType.Admin && (
+            <RedeemTaskButton task={task} />
+          ) }
         </CardFooter>
       </Link>
     </Card>
