6f520e1dfd6e1006ebd02d70026167d34410586d
Malek Hamza <malek.hamza@gmx.de>
2023-09-29 09:57:22 +0200

    Replace auth route with decent ssr config

--- frontend/app/auth-context.tsx
+++ frontend/app/auth-context.tsx
@@ -1,50 +1,13 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { AuthProvider, AuthProviderProps } from "react-oidc-context";
 
-import { AuthProvider } from "react-oidc-context";
-
-import { Icons } from "@/components/icons";
 
 export interface AuthContextProps {
   children: React.ReactNode;
+  props: AuthProviderProps
 }
 
-interface KeycloakConfig {
-  authority?: string;
-  client_id?: string;
-}
-
-async function getKeycloakConfig() {
-  const result = await fetch("/api/v1/auth/config");
-  const data = await result.json();
-  return data as KeycloakConfig;
-}
-
-export default function AuthContext({ children }: AuthContextProps) {
-  const [config, setConfig] = useState<KeycloakConfig | undefined>();
-
-  const fetchConfig = async () => {
-    const data = await getKeycloakConfig();
-    setConfig(data);
-  };
-
-  useEffect(() => {
-    fetchConfig();
-  }, []);
-
-  if (!config) {
-    return (
-      <div className="grid h-screen w-full place-items-center">
-        <Icons.spinner className="animate-spin" />{" "}
-      </div>
-    );
-  } else {
-    const oidcConfig = {
-      authority: config.authority ?? "",
-      client_id: config.client_id ?? "",
-      redirect_uri: "/",
-    };
-    return <AuthProvider {...oidcConfig}>{children}</AuthProvider>;
-  }
+export default function AuthContext({ children, props }: AuthContextProps) {
+  return <AuthProvider {...props}>{children}</AuthProvider>;
 }
